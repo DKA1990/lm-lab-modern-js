ca3903fce4ca971c21fa76e8553219d4b52b2b80
danielatwell2k5@msn.com <danielatwell2k5@msn.com>
2022-12-12 15:39:59 +0000

    Exercises complete

--- modern_javascript_exercises/rest_parameters/rest_parameters.js
+++ modern_javascript_exercises/rest_parameters/rest_parameters.js
@@ -1,10 +1,9 @@
 // Instructions can be found in rest_parameters.md
 
-function add(a, b) {
-  // Add a loop here
-  return a + b;
+function add(...arr) {  
+  return arr.reduce((acc, curr) => acc + curr);
 }
 
-add(1, 2, 3, 4, 5);
+//add(1, 2, 3, 4, 5);
 
-module.exports = add;
+module.exports.add = add;
